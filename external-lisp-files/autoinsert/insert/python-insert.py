f692c0ea96f55c7082de294c5b95272e38e4c127
Matthew Savoie <github@flamingbear.com>
2013-01-24 13:21:15 -0700

    changed the default python template

--- external-lisp-files/autoinsert/insert/python-insert.py
+++ external-lisp-files/autoinsert/insert/python-insert.py
@@ -1,8 +1,18 @@
-##############################################################################
-#
-# %f   -- %@
-#
-# %M/%D/%y  %U  %a
-# National Snow & Ice Data Center, University of Colorado, Boulder
-# Copyright (C) <%Y> University of Colorado.
-##############################################################################
+import argparse as ap
+
+
+def parse_arguments():
+    parser = ap.ArgumentParser(
+        description="Put description here."
+    )
+    parser.add_argument('first_argument',
+                        help='description of first_argument.')
+    parser.add_argument('second_argument',
+                        help='describe second_argument')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    first_argument = args.first_argument
+    second_argument = args.second_argument
