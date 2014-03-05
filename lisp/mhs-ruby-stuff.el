8ea80d9b18dfba9da9346897f45b6cc1709d5fe2
Matt Savoie <github@flamingbear.com>
2014-03-05 08:48:54 -0700

    added Vagrant.* files to ruby mode

--- lisp/mhs-ruby-stuff.el
+++ lisp/mhs-ruby-stuff.el
@@ -60,6 +60,7 @@ Report bugs to: Matt Savoie <emacs@flamingbear.com>")
 (autoload 'ruby-mode "ruby-mode" "Major mode for ruby files" t)
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile.*" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake" . ruby-mode))
 (add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
 
