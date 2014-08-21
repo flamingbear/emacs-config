f811eed509b24f300b36f26af8f0596c97fbc564
Matt Savoie <savoie@nsidc.org>
2014-08-21 08:09:29 -0600

    Nuke some possibly obsolete mac only key bindings

--- emacs-keybinds.el
+++ emacs-keybinds.el
@@ -136,8 +136,6 @@
 
 ;; Options for Macintosh Laptop
 ;;------------------------------
-(setq mac-option-key-is-meta nil)
-(setq mac-command-key-is-meta t)
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'super)
 
