278047ad8ec5ffb1e3fdea7cdc8d0892c49a6f50
Matt Savoie <github@flamingbear.com>
2015-01-19 17:32:51 -0700

    Start of test.
    Still need to work on default virtual environment.
    Would a simple each time manual thing work as opposed to my one time
    adding a .dir-locals.el file.

--- lisp/mhs-python.el
+++ lisp/mhs-python.el
@@ -4,31 +4,31 @@
 
 ;; We're gonna need us a Python mode
 (require 'python)
-
+(elpy-enable)
 ;; Python is a dev mode
 (add-hook 'python-mode-hook 'run-dev-hook)
 
 
-(require 'virtualenvwrapper)
-(venv-initialize-interactive-shells) ;; if you want interactive shell support
-(venv-initialize-eshell) ;; if you want eshell support
-(setq venv-location "~/.virtual_envs/")
+;; (require 'virtualenvwrapper)
+;; (venv-initialize-interactive-shells) ;; if you want interactive shell support
+;; (venv-initialize-eshell) ;; if you want eshell support
+;; (setq venv-location "~/.virtual_envs/")
 
 
 
 ;; Be able to run nose tests with various keybindings
-(require 'nose)
-(add-hook 'python-mode-hook
-          (lambda ()
-            (hack-local-variables)
-            (when (boundp 'project-venv-name)
-              (venv-workon project-venv-name))
-            (local-set-key "\C-ca" 'nosetests-all)
-            (local-set-key "\C-cm" 'nosetests-module)
-            (local-set-key "\C-c." 'nosetests-one)
-            (local-set-key "\C-cpa" 'nosetests-pdb-all)
-            (local-set-key "\C-cpm" 'nosetests-pdb-module)
-            (local-set-key "\C-cp." 'nosetests-pdb-one)))
+;; (require 'nose)
+;; (add-hook 'python-mode-hook
+;;           (lambda ()
+;;             (hack-local-variables)
+;;             (when (boundp 'project-venv-name)
+;;               (venv-workon project-venv-name))
+;;             (local-set-key "\C-ca" 'nosetests-all)
+;;             (local-set-key "\C-cm" 'nosetests-module)
+;;             (local-set-key "\C-c." 'nosetests-one)
+;;             (local-set-key "\C-cpa" 'nosetests-pdb-all)
+;;             (local-set-key "\C-cpm" 'nosetests-pdb-module)
+;;             (local-set-key "\C-cp." 'nosetests-pdb-one)))
 
 
 ;; the venv-workon above lets you use .dir-local.el files to control the virtual env and activate it...
@@ -37,9 +37,9 @@
 ;;  (python-mode . ((project-venv-name . "local"))))
 
 ;; Use the Python force, my young padawan learner
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:setup-keys t)
-(setq jedi:complete-on-dot t)
+;; (add-hook 'python-mode-hook 'jedi:setup)
+;; (setq jedi:setup-keys t)
+;; (setq jedi:complete-on-dot t)
 
 (add-hook 'ein:connect-mode-hook 'ein:jedi-setup)
 
