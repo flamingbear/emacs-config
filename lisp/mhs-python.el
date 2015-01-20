cba65650c9cb859b3961258b9c272bf30eb34bc9
Matt Savoie <savoie@nsidc.org>
2015-01-20 11:32:06 -0700

    Turn of flymake in favor of flycheck.

--- lisp/mhs-python.el
+++ lisp/mhs-python.el
@@ -1,12 +1,17 @@
-
-
-;; Below: Stolen from KWB totally
+;; Below: Stolen from KWB totally^H^H^H^H^H mostly.
 
 ;; We're gonna need us a Python mode
 (require 'python)
 (require 'elpy)
 (require 'pyvenv)
 (elpy-enable)
+
+;; don't use flymake (elpy default), use flycheck
+;; from: https://github.com/jorgenschaefer/elpy/issues/137
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
 ;; track virtual environments if they are set dir locally
 (setq pyvenv-tracking-mode 't)
 
@@ -22,18 +27,18 @@
 
 
 ;; Be able to run nose tests with various keybindings
-;; (require 'nose)
-;; (add-hook 'python-mode-hook
-;;           (lambda ()
-;;             (hack-local-variables)
-;;             (when (boundp 'project-venv-name)
-;;               (venv-workon project-venv-name))
-;;             (local-set-key "\C-ca" 'nosetests-all)
-;;             (local-set-key "\C-cm" 'nosetests-module)
-;;             (local-set-key "\C-c." 'nosetests-one)
-;;             (local-set-key "\C-cpa" 'nosetests-pdb-all)
-;;             (local-set-key "\C-cpm" 'nosetests-pdb-module)
-;;             (local-set-key "\C-cp." 'nosetests-pdb-one)))
+(require 'nose)
+(add-hook 'python-mode-hook
+          (lambda ()
+            (hack-local-variables)
+            (when (boundp 'project-venv-name)
+              (venv-workon project-venv-name))
+            (local-set-key "\C-ca" 'nosetests-all)
+            (local-set-key "\C-cm" 'nosetests-module)
+            (local-set-key "\C-c." 'nosetests-one)
+            (local-set-key "\C-cpa" 'nosetests-pdb-all)
+            (local-set-key "\C-cpm" 'nosetests-pdb-module)
+            (local-set-key "\C-cp." 'nosetests-pdb-one)))
 
 
 ;; the venv-workon above lets you use .dir-local.el files to control the virtual env and activate it...
