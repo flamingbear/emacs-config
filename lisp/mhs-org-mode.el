f22cd0dac2029c94eba1c2d9f318577c8fb5ecd5
Matt Savoie <savoie@nsidc.org>
2015-01-21 12:11:43 -0700

    org-capture

--- lisp/mhs-org-mode.el
+++ lisp/mhs-org-mode.el
@@ -51,8 +51,7 @@
 (define-key global-map "\C-ca" 'org-agenda)
 (define-key global-map "\C-cb" 'org-iswitchb)
 
-;; (require 'org-remember)
-;; (org-remember-insinuate)
+(require 'org-capture)
 (setq org-directory "~/Dropbox/orgs")
 (setq org-default-notes-file (concat org-directory "/notes.org"))
 
