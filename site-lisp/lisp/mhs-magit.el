e0a945790a753d5bb70f8098443b06130ecd06a3
Matt Savoie <savoie@nsidc.org>
2015-03-19 08:46:48 -0600

    Add bitbucket to pull request visit.

--- site-lisp/lisp/mhs-magit.el
+++ site-lisp/lisp/mhs-magit.el
@@ -28,19 +28,33 @@
 
 ;;  TODO integrate bitbucket:
 ;; "git@bitbucket.org:nsidc/extended_generic_swath.git"
+;;  new PR: https://bitbucket.org/nsidc/measures-byu-vm/pull-request/new
 
 ;; http://endlessparentheses.com/easily-create-github-prs-from-magit.html?source=rss
 (defun endless/visit-pull-request-url ()
   "Visit the current branch's PR on Github."
   (interactive)
+  (let ((repo (magit-get "remote" (magit-get-current-remote) "url")))
+    (if (string-match "github\\.com" repo)
+        (visit-gh-pull-request repo)
+      (visit-bb-pull-request repo))))
+
+(defun visit-gh-pull-request (repo)
   (browse-url
    (format "https://github.com/%s/compare/%s"
-     (replace-regexp-in-string
-      "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1"
-      (magit-get "remote"
-                 (magit-get-current-remote)
-                 "url"))
-     (magit-get-current-branch))))
+           (replace-regexp-in-string
+            "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1"
+            repo)
+           (magit-get-current-branch))))
+
+;; This could be changed to not look at the new pull requests.
+;; https://bitbucket.org/nsidc/measures-byu-vm/pull-request/new
+(defun visit-bb-pull-request (repo)
+  (browse-url
+   (format "https://bitbucket.org/%s/pull-request/new"
+           (replace-regexp-in-string
+            "\\`.+bitbucket\\.org:\\(.+\\)\\.git\\'" "\\1"
+            repo))))
 
 (eval-after-load 'magit
   '(define-key magit-mode-map "V"
