0e972310d7c48121d5ee552494adbdd7b33db87b
Matt Savoie <savoie@nsidc.org>
2015-05-04 09:55:03 -0600

    moved pyenv
    stoped savehist

--- site-lisp/savoie/emacs-extras.el
+++ site-lisp/savoie/emacs-extras.el
@@ -36,9 +36,10 @@
 (setq fill-column 80)
 
 ;; Save minibuffer history
-;; TODO [MHS, 2015-04-23] This isn't working right now
+;; TODO [MHS, 2015-04-23] This isn't working right now, can't quit emacs.
 ;; If you get hung up.  (setq savehist-mode 'nil)
-(savehist-mode 1)
+                                        ; (savehist-mode 1)
+(setq savehist-mode 'nil)
 (setq history-length 1000)
 
 ;; Easily NavigateSillyCased words
