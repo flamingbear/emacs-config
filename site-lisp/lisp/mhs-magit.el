4da08a5305c8478d0254a8d1d299c2e3939bd7d9
Matt Savoie <savoie@nsidc.org>
2015-07-22 11:17:04 -0600

    Browse to internal CI machines

--- site-lisp/lisp/mhs-magit.el
+++ site-lisp/lisp/mhs-magit.el
@@ -40,10 +40,26 @@
             "\\`.+bitbucket\\.org:\\(.+\\)\\.git\\'" "\\1"
             repo))))
 
+
+;; browse to you CI machine if it is created normally.
+
+(defun mhs/visit-ci-machine ()
+  "browse to this project's CI jenkins instance."
+  (interactive)
+  (browse-url (format "http://ci.%s.apps.int.nsidc.org:8080/"
+                      (replace-regexp-in-string
+                       "\\`.+bitbucket\\.org:nsidc\/\\(.+\\)\\.git\\'" "\\1"
+                       (magit-get "remote" (magit-get-remote) "url")))))
+
+
 ;; visit PR for github or bitbucket repositories with "V"
 (eval-after-load 'magit
   '(define-key magit-mode-map "v"
      #'endless/visit-pull-request-url))
+
+(eval-after-load 'magit
+  '(define-key mhs-map "c"
+     #'mhs/visit-ci-machine))
 
 
 (provide 'mhs-magit)
