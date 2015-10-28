50a6e76c4fc195b19243d10eb4e96f43ea456378
Matt Savoie <savoie@nsidc.org>
2015-10-28 10:35:52 -0600

    Update sii navigation.

--- site-lisp/lisp/mhs-sii.el
+++ site-lisp/lisp/mhs-sii.el
@@ -42,166 +42,95 @@
 Report bugs to: Matt Savoie <emacs@flamingbear.com>")
 
 
-(defvar sii-current-proj-top (getenv "NRTSI_PROJ_TOP")
-  "Current Project Top Dir")
+(defvar sii-proj-top (getenv "NRTSI_PROJ_TOP") "Current Project Top Dir.")
+
+(defvar sii-output-root (getenv "OUTPUT_ROOT") "Output_root location.")
 
 (defun sii-set-topdir ()
   (interactive)
-  (setq sii-current-proj-top (file-name-directory (buffer-file-name))))
-
-;; Functions to set the environment.
-(defun df17 ()
-  "Set the build to F17 development"
-  (interactive)
-  (setenv "BUILD" "F17_dev" )
-  (setenv "NRTSI_PROJ_TOP" "/projects/NRTSI-G/F17_dev" )
-  (setenv "NRTSI_WEBBASE" "/projects/NRTSI-G/F17_dev/website_base")
-  (setq sii-current-proj-top (getenv "NRTSI_PROJ_TOP"))
-  (setq sii-current-web (getenv "NRTSI_WEBBASE")))
-
-(defun tf17 ()
-  "Set the build to F17 testing"
-  (interactive)
-  (setenv "BUILD" "F17_test" )
-  (setenv "NRTSI_PROJ_TOP" "/projects/NRTSI-G/F17_test" )
-  (setenv "NRTSI_WEBBASE" "/disks/testsnowtest/live/data/seaice_index/")
-  (setq sii-current-proj-top (getenv "NRTSI_PROJ_TOP"))
-  (setq sii-current-web (getenv "NRTSI_WEBBASE")))
-
-(defun pf17 ()
-  "Set the build to F17 production"
-  (interactive)
-  (setenv "BUILD" "F17_prod" )
-  (setenv "NRTSI_PROJ_TOP" "/projects/NRTSI-G/F17_prod" )
-  (setenv "NRTSI_WEBBASE" "/disks/production/live/data/seaice_index/")
-  (setq sii-current-proj-top (getenv "NRTSI_PROJ_TOP"))
-  (setq sii-current-web (getenv "NRTSI_WEBBASE")))
+  (setq sii-proj-top (file-name-directory (buffer-file-name))))
 
 
-
-;; Functions for moving around the environment
 (defun nrtsi-proj-top ()
-  (setq nrtsi-proj-top-dir sii-current-proj-top)
-  nrtsi-proj-top-dir )
+  "Functions for moving around the environment."
+  sii-proj-top )
 
 
-(defun nrtsitop-subdir ( subdir )
-  "Jump to a subdir of the NRTSTITOP"
-  (nrtsi-proj-top-subdir (concat "nrtsig" "/" subdir)))
+(defun nrtsi-out-top ()
+  "Functions for moving around the environment."
+  sii-output-root )
 
 
-(defun nrtsi-proj-top-subdir (subdir)
-  "jump to a subdirecotry of the nrtsi-proj-top"
-  (setq dir-to-jump (concat (nrtsi-proj-top) (concat "/" subdir) ))
-  (dired dir-to-jump))
+(defun nrtsi-proj-top-subdir ( subdir )
+  "Jump to a SUBDIR of the NRTSTI-PROJ-TOP."
+  (concat (nrtsi-proj-top) "/" subdir))
 
+(defun nrtsi-out-subdir ( subdir )
+  "Jump to a SUBDIR of the OUTPUT_ROOT."
+  (concat (nrtsi-out-top) "/" subdir))
 
-(defun mhs-idl ()
-  "Jump to the current mhs-idl library"
+(defun sii-mhs-idl ()
+  "Jump to the current mhs-idl library."
   (interactive)
-  (nrtsi-proj-top-subdir "extra_idl_libs/idl_util"))
-
-(defun sii-src ()
-  "Jump to the current mhs-src library"
-  (interactive)
-  (nrtsi-proj-top-subdir "nrt_seaice/src/scripts"))
-
-(defun sii-regression ()
-  "Jump to the current mhs-regression library"
-  (interactive)
-  (nrtsi-proj-top-subdir "seaice_index/idl/regression"))
-
-(defun sii-regenerate ()
-  "Jump to the current mhs-regression library"
-  (interactive)
-  (nrtsi-proj-top-subdir "seaice_index/idl/one_off_specials/regenerate_data"))
+  (dired (nrtsi-proj-top-subdir "extra_idl_libs/idl_util")))
 
 (defun sii-top ()
-  "Jump to current project top"
+  "Jump to current project top."
   (interactive)
   (dired (nrtsi-proj-top)))
 
-(defun sii-results ()
-  "Jump to the nrtsi results directory"
-  (interactive)
-  ( nrtsitop-subdir "results" ))
-
 (defun sii-website ()
-  "Jump to the nrtsi results directory"
+  "Jump to the nrtsi results directory."
   (interactive)
-  (dired sii-current-web))
+  (dired (nrtsi-out-subdir "website")))
 
 
 (defun sii-ancillary ()
-  "Jump to the nrtsi results directory"
+  "Jump to the nrtsi results directory."
   (interactive)
-  ( nrtsitop-subdir "ancillary" ))
+  (dired  (nrtsi-out-subdir "nrtsig/ancillary" )))
 
 (defun sii-movies ()
-  "jump to the movie dir"
+  "Jump to the movie dir."
   (interactive)
-  (nrtsi-proj-top-subdir "seaice_index/idl/animations/movies"))
+  (dired (nrtsi-proj-top-subdir "/seaice_index/idl/animations/movies")))
 
 (defun sii-lib ()
-  "Jump to the nrtsi results directory"
+  "Jump to the nrtsi results directory."
   (interactive)
-  ( nrtsitop-subdir "lib" ))
+  (dired (nrtsi-out-subdir "nrtsig/lib" )))
 
 (defun sii-logs ()
-  "Jump to the nrtsi logs directory"
+  "Jump to the nrtsi logs directory."
   (interactive)
-  (nrtsitop-subdir "logs" ))
+  (dired (nrtsi-out-subdir "nrtsig/logs" )))
 
-(defun sii-bin ()
-  "jump to the nrtsig bin directory"
-  (interactive)
-  (nrtsitop-subdir "bin"))
-
-(defun sii-comm ()
-  "jump to the nrtsig comm directory"
-  (interactive)
-  (nrtsitop-subdir "comm"))
 
 (defun sii-libs ()
-  "jump to library locs"
+  "Jump to library locs."
   (interactive)
-  (nrtsi-proj-top-subdir "seaice_index/idl/sii_libs"))
+  (dired (nrtsi-proj-top-subdir "seaice_index/idl/sii_libs")))
 
 (defun sii-idl ()
-  "Jump to the nrtsi IDL directory"
+  "Jump to the nrtsi IDL directory."
   (interactive)
-  ( nrtsi-proj-top-subdir  "seaice_index/idl" ))
+  (dired ( nrtsi-proj-top-subdir  "seaice_index/idl" )))
 
 (defun sii-perl ()
-  "Jump to the nrtsi perl directory"
+  "Jump to the nrtsi perl directory."
   (interactive)
-  ( nrtsi-proj-top-subdir  "seaice_index/perl" ))
-
-(defun sii-crons ()
-  "jump to the nrtsi crons directory"
-  (interactive)
-  ( nrtsi-proj-top-subdir "nrt_seaice/cron_info"))
-
-(defun sii-nrt ()
-  "jump to the nrt_seaice directory"
-  (interactive)
-  ( nrtsi-proj-top-subdir "nrt_seaice"))
+  (dired ( nrtsi-proj-top-subdir  "seaice_index/perl" )))
 
 (defun sii-images ()
-  "jump to the seaice_index/idl/images  directory"
+  "Jump to the images directory."
   (interactive)
-  (nrtsi-proj-top-subdir "seaice_index/idl/images"))
+  (dired (nrtsi-proj-top-subdir "seaice_index/idl/images")))
 
 (defun sii-extents ()
-  "jump to the seaice_index/idl/extent_ts_output  directory"
+  "Jump to the extent_ts_output directory."
   (interactive)
-  (nrtsi-proj-top-subdir "seaice_index/idl/extent_ts_output"))
+  (dired (nrtsi-out-subdir "extent_ts_output")))
 
-
-(defun sii-build-string ()
-  "Insert the string that sets the environment"
-  (interactive)
-  (insert '"export BUILD=" (getenv "BUILD")) )
 
 (provide 'mhs-sii)
 
