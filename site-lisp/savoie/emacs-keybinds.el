c50d0289ea31d7ac0f32e386be3dc1d98fc9e6ff
Matt Savoie <savoie@nsidc.org>
2016-05-25 13:20:35 -0600

    hydra the multiple-cursors.
    https://github.com/abo-abo/hydra/wiki/multiple-cursors

--- site-lisp/savoie/emacs-keybinds.el
+++ site-lisp/savoie/emacs-keybinds.el
@@ -148,10 +148,31 @@
 ;;   (global-set-key (kbd "C-*") 'mark-all-like-this))
 
 (when (try-require 'multiple-cursors)
-  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
-  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-  (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-  (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines))
+  ;; (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+  ;; (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+  ;; (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+  ;; (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+  (defhydra multiple-cursors-hydra (mhs-map "<f9>")
+    "
+     ^Up^            ^Down^        ^Other^
+----------------------------------------------
+[_p_]   Next    [_n_]   Next    [_l_] Edit lines
+[_P_]   Skip    [_N_]   Skip    [_a_] Mark all
+[_M-p_] Unmark  [_M-n_] Unmark  [_r_] Mark by regexp
+^ ^             ^ ^             [_q_] Quit
+"
+    ("l" mc/edit-lines :exit t)
+    ("a" mc/mark-all-like-this :exit t)
+    ("n" mc/mark-next-like-this)
+    ("N" mc/skip-to-next-like-this)
+    ("M-n" mc/unmark-next-like-this)
+    ("p" mc/mark-previous-like-this)
+    ("P" mc/skip-to-previous-like-this)
+    ("M-p" mc/unmark-previous-like-this)
+    ("r" mc/mark-all-in-region-regexp :exit t)
+    ("q" nil))
+
+  )
 
 (when (boundp 'mhs-searchmap)
   (define-key mhs-searchmap "s" 'rgrep-fullscreen))
