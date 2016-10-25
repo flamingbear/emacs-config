1d59226686f297932fe5241a622e57f9fb1d13ba
Matt Savoie <savoie@nsidc.org>
2016-10-25 09:02:57 -0600

    add stable melpa

--- init.el
+++ init.el
@@ -39,6 +39,7 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (package-initialize)
