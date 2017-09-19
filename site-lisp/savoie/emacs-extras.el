caf0fa9d8347cab23856b575369f2bf0564e2149
Matt Savoie <savoie@nsidc.org>
2017-09-19 17:23:06 -0600

    HACK: fix for projectile project discovery.

--- site-lisp/savoie/emacs-extras.el
+++ site-lisp/savoie/emacs-extras.el
@@ -147,7 +147,26 @@
   :ensure t
   :config
   (projectile-global-mode t)
-  (setq projectile-completion-system 'ivy))
+  (setq projectile-completion-system 'ivy)
+  ;; HACK until this is fixed: https://github.com/bbatsov/projectile/issues/1165
+  (defun projectile-discover-projects-in-directory (directory)
+  "Discover any projects in DIRECTORY and add them to the projectile cache.
+This function is not recursive and only adds projects with roots
+at the top level of DIRECTORY."
+  (interactive
+   (list (read-directory-name "Starting directory: ")))
+  (let ((subdirs (directory-files directory t)))
+    (mapcar
+     (lambda (dir)
+       (when (and (file-directory-p dir)
+                  (not (member (file-name-nondirectory dir) '(".." "."))))
+         (let ((default-directory dir)
+               (projectile-cached-project-root dir))
+           (when (projectile-project-p)
+             (projectile-add-known-project (projectile-project-root))))))
+     subdirs)))
+
+  )
 
 (use-package grep
   :defer 3
