9ace43985f526ddc1ef43476f50fca3600555f58
Matt Savoie <savoie@nsidc.org>
2017-12-13 11:09:51 -0700

    Try Wilfred's helpful
    Better emacs help.

--- site-lisp/savoie/emacs-extras.el
+++ site-lisp/savoie/emacs-extras.el
@@ -483,6 +483,23 @@
   )
 
 
+(use-package helpful
+  ;; Helpful is an alternative to the built-in Emacs help that provides much more contextual information.
+
+
+  :ensure t
+  :config
+  ;; Note that the built-in `describe-function' includes both functions
+  ;; and macros. `helpful-function' is functions only, so we provide
+  ;; `helpful-callable' as a drop-in replacement.
+  (global-set-key (kbd "C-h f") #'helpful-callable)
+
+  (global-set-key (kbd "C-h v") #'helpful-variable)
+  (global-set-key (kbd "C-h k") #'helpful-key)
+  ;; I also recommend you configure helpful-at-point to a convenient keybinding:
+  (global-set-key (kbd "C-c C-.") #'helpful-at-point) )
+
+
 ;; Set registers to things that I type zillions of times.
 ;; ------------------------------------------------------
 
