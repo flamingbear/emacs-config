a40a9fa1bfd73e933466efe771454044162847ba
Matt Savoie <savoie@nsidc.org>
2018-03-09 08:37:13 -0700

    The wrong way to fix this.
    But it's fixed.  About to re-re-fix.

--- site-lisp/lisp/mhs-python.el
+++ site-lisp/lisp/mhs-python.el
@@ -1,4 +1,4 @@
-;; Below: Stolen from KWB totally^H^H^H^H^H mostly.
+;; Below: Stolen from KWB totally^H^H^H^H^H mostly (well, originally anyway).
 
 ;; We're gonna need us a Python mode
 (use-package python
@@ -41,7 +41,49 @@
               (local-set-key "\C-c." 'nosetests-one)
               (local-set-key "\C-cpa" 'nosetests-pdb-all)
               (local-set-key "\C-cpm" 'nosetests-pdb-module)
-              (local-set-key "\C-cp." 'nosetests-pdb-one))))
+              (local-set-key "\C-cp." 'nosetests-pdb-one)))
+
+  ;; Added this so that full path to nosetests would work.  This just replaces
+  ;; nosetests with full/path/nosetests.  I don't know why it wouldn't work
+  ;; before.
+  (defun run-nose (&optional tests debug failed)
+    "run nosetests"
+    (setq nose--last-run-params (list tests debug failed))
+
+    (let* ((nose (executable-find "nosetests"))
+	   (where (or nose-local-project-root (nose-find-project-root)))
+	   (args (concat (if debug "--pdb" "")
+			 " "
+			 (if failed "--failed" "")))
+	   (tnames (if tests tests "")))
+      (if (not where)
+	  (error
+	   (format (concat "abort: nosemacs couldn't find a project root, "
+			   "looked for any of %S") nose-project-root-files)))
+
+      ;; Execute nosetests and display the result in a compilation buffer.
+      ;;
+      ;; Store the active project root in a buffer-local variable, so that nose
+      ;; can invoked from it by the user after execution is complete. This is
+      ;; necessary because the compilation buffer doesn't have a filename from
+      ;; which it could be discovered.
+      (funcall (if debug
+		   'pdb
+		 '(lambda (command)
+		    (let ((compilation-error-regexp-alist
+			   '(("  File \"\\(.*\\)\", line \\([0-9]+\\), in test_" 1 2))))
+		      (save-current-buffer
+			(set-buffer (compilation-start command
+						       nil
+						       (lambda (mode) (concat "*nosetests*"))))
+			(setq-local nose-local-project-root where)))))
+	       (format
+		(concat "%s "
+			(if nose-use-verbose "-v " "")
+			"%s -w %s -c %ssetup.cfg %s")
+		nose args where where tnames)))
+    )
+  )
 
 
 ;; Stole this:  Might be useful if we go to pytest.
