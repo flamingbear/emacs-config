284022404099c4cede5a35406fdd3dfd34c47ed7
Matt Savoie <savoie@nsidc.org>
2019-02-24 17:27:35 -0700

    Something is adding this and the doulbe elpa seems to be a problem.
    I don't really know what that would be, but I don't care.

--- init.el
+++ init.el
@@ -41,8 +41,8 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+;; (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
