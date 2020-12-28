5853001cfc69a57427e33d7cf28571ee9a85b70d
Matt Savoie <savoie@nsidc.org>
2020-12-28 13:18:10 -0700

    nuke nose.

--- site-lisp/lisp/mhs-python.el
+++ site-lisp/lisp/mhs-python.el
@@ -29,34 +29,6 @@
   (add-hook 'elpy-mode-hook 'flycheck-mode))
 
 
-;; Be able to run nose tests with various keybindings
-(use-package nose
-  :ensure t
-  :config
-  (add-hook 'python-mode-hook
-            (lambda ()
-              (local-set-key "\C-ca" 'nosetests-all)
-              (local-set-key "\C-cm" 'nosetests-module)
-              (local-set-key "\C-c." 'nosetests-one)
-              (local-set-key "\C-cpa" 'nosetests-pdb-all)
-              (local-set-key "\C-cpm" 'nosetests-pdb-module)
-              (local-set-key "\C-cp." 'nosetests-pdb-one)))
-
-  ;; This could probably be even smarter.
-  (defun nose-find-test-runner ()
-    (message
-     (let ((result
-	    (reduce '(lambda (x y) (or x y))
-		    (mapcar 'nose-find-test-runner-names nose-project-names))))
-       (if result
-	   result
-	 (if (executable-find "nosetests")
-	     (executable-find "nosetests")
-	   nose-global-name))))
-    )
-)
-
-
 (use-package ein
   :ensure t
   :init
