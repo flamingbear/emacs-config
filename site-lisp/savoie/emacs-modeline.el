783e8e22a948371203253e52f90b06f45706e352
Matt Savoie <savoie@nsidc.org>
2021-02-08 16:20:50 -0700

    Doom modeline hangs

--- site-lisp/savoie/emacs-modeline.el
+++ site-lisp/savoie/emacs-modeline.el
@@ -1,7 +1,34 @@
 ;; This sets up fancy modelines on non-terminal displays.
+;; This sets up fancy modelines on non-terminal displays.
 
-(use-package doom-modeline
+
+(use-package spaceline
   :ensure t
-  :init (doom-modeline-mode 1))
+  :init
+  (setq powerline-default-separator 'wave)
+  :config
+  (require 'spaceline-config)
+  (spaceline-emacs-theme)
+  (setq powerline-image-apple-rgb t)
+  (diminish 'auto-revert-mode)
+
+  ;; If you want the leftmost to match your custom faces, use this.
+  ;; (defface mhs-spaceline-highlight-face
+  ;;   `((t . (:background ,my-modeline-fg-color :foreground ,my-modeline-bg-color :inherit 'mode-line)))
+  ;;   "Default highlight face for spaceline.")
+  ;; (defun mhs-spaceline-highlight-face-default ()
+  ;;   "The default highlight face function.
+  ;;      Set `spaceline-highlight-face-func' to
+  ;;      `mhs-spaceline-highlight-face-default' to use this."
+  ;;   'mhs-spaceline-highlight-face)
+  ;; (setq spaceline-highlight-face-func 'mhs-spaceline-highlight-face-default)
+  (setq spaceline-highlight-face-func 'spaceline-highlight-face-modified)
+  )
+
+(provide 'emacs-modeline)
+
+;; (use-package doom-modeline
+;;   :ensure t
+;;   :init (doom-modeline-mode 1))
 
 (provide 'emacs-modeline)
