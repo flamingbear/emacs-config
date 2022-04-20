90e8879134e01ac66dc355c359b08859d190c48c
Matt Savoie <savoie@colorado.edu>
2022-04-20 16:43:24 -0600

    Update mhs-magit with commit hook
    Now when you make a commit, either the parsed ticket from the branch or the
    value in mhs-jira--current-ticket-number will be inserted two lines down in
    your message.

--- site-lisp/lisp/mhs-magit.el
+++ site-lisp/lisp/mhs-magit.el
@@ -20,77 +20,26 @@
   (setq magit-push-always-verify nil)
   (setq magit-diff-refine-hunk 'all)
 
-  ;; http://endlessparentheses.com/automatically-configure-magit-to-access-github-prs.html
-  (defun endless/add-PR-fetch ()
-  "If refs/pull is not defined on a GH repo, define it."
-  (let ((fetch-address
-         "+refs/pull/*/head:refs/pull/origin/*")
-        (magit-remotes
-         (magit-get-all "remote" "origin" "fetch")))
-    (unless (or (not magit-remotes)
-                (member fetch-address magit-remotes))
-      (when (string-match
-             "github" (magit-get "remote" "origin" "url"))
-        (magit-git-string
-         "config" "--add" "remote.origin.fetch"
-         fetch-address)))))
+  ;; https://emacs.stackexchange.com/a/44685/613
+  (defun mhs/parse-current-branch ()
+    (let ((ISSUEKEY "[[:upper:]]+-[[:digit:]]+"))
+      (when (string-match-p ISSUEKEY (magit-get-current-branch))
+	(insert
+	 (replace-regexp-in-string
+	  (concat ".*?\\(" ISSUEKEY "\\).*")
+	  "- \\1: "
+	  (magit-get-current-branch))))))
+  ;; return the ticket parsed from the branch or my current ticket number
+  (defun mhs/current-ticket ()
+    (or (mhs/parse-current-branch)
+	mhs-jira--current-ticket-number))
 
-  (add-hook 'magit-mode-hook #'endless/add-PR-fetch)
+  (defun my-git-commit-setup ()
+    (save-excursion
+      (insert (concat "\n\n" (mhs/current-ticket)))))
 
-  ;; Code:
-  ;; Originally idea for Github PR Stolen (well) from here:
-  ;; http://endlessparentheses.com/easily-create-github-prs-from-magit.html?source=rss
+  (add-hook 'git-commit-setup-hook 'my-git-commit-setup)
 
-  (defun endless/visit-pull-request-url ()
-    "Visit the current branch's PR on Github."
-    (interactive)
-    (let ((repo (magit-get "remote" (magit-get-remote) "url")))
-      (if (string-match "github\\.com" repo)
-          (visit-gh-pull-request repo)
-        (visit-bb-pull-request repo))))
-
-
-
-  (defun visit-gh-pull-request (repo)
-    "Visit the current branch's PR on Github."
-    (interactive)
-    (browse-url
-     (format "https://github.com/%s/pull/new/%s"
-             (replace-regexp-in-string
-              "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1"
-              repo)
-             (magit-get-current-branch))))
-
-
-
-  ;; Bitbucket pull requests are kinda funky, it seems to try to just do the
-  ;; right thing, so there's no branches to include.
-  ;; https://bitbucket.org/nsidc/measures-byu-vm/pull-request/new
-  (defun visit-bb-pull-request (repo)
-    (browse-url
-     (format "https://bitbucket.org/%s/pull-request/new"
-             (replace-regexp-in-string
-              "\\`.+bitbucket\\.org:\\(.+\\)\\.git\\'" "\\1"
-              repo))))
-
-  ;; browse to you CI machine if it is created normally.
-  (defun mhs/visit-ci-machine ()
-    "browse to this project's CI jenkins instance."
-    (interactive)
-    (browse-url (format "http://ci.%s.apps.int.nsidc.org:8080/"
-                        (replace-regexp-in-string
-                         "\\`.+bitbucket\\.org:nsidc\/\\(.+\\)\\.git\\'" "\\1"
-                         (magit-get "remote" (magit-get-remote) "url")))))
-
-
-  ;; visit PR for github or bitbucket repositories with "V"
-  (eval-after-load 'magit
-    '(define-key magit-mode-map "v"
-       #'endless/visit-pull-request-url))
-
-  (eval-after-load 'magit
-    '(define-key mhs-map "c"
-       #'mhs/visit-ci-machine))
 
   (setq magit-completing-read-function 'ivy-completing-read)
   )
