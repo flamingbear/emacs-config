1ecc274c5543391bddb38a27932f65d700a5fa53
Matt Savoie <savoie@nsidc.org>
2024-03-02 21:31:34 -0700

    dall-e too

--- site-lisp/savoie/emacs-extras.el
+++ site-lisp/savoie/emacs-extras.el
@@ -561,8 +561,13 @@ the current buffer."
   :config
   (setq chatgpt-shell-model-version "gpt-3.5-turbo-0125")
   (setq chatgpt-shell-system-prompt 1)
+  (require 'dall-e-shell)
 
   (setq chatgpt-shell-openai-key
+	(lambda ()
+          (auth-source-pick-first-password :host "api.openai.com")))
+
+  (setq dall-e-shell-openai-key
 	(lambda ()
           (auth-source-pick-first-password :host "api.openai.com")))
 
@@ -609,6 +614,7 @@ the current buffer."
 100%.")
    chatgpt-shell-system-prompts)
   )
+
 
 ;; Set up copilot
 (defvar bootstrap-version)
