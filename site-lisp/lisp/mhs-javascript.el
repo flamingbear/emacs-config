50d43f40b81cf4ddaf1c0e4875b165e9a6e27294
Matt Savoie <matthew.savoie@colorado.edu>
2026-01-29 09:35:19 -0700

    Nuke old js packages

--- site-lisp/lisp/mhs-javascript.el
+++ site-lisp/lisp/mhs-javascript.el
@@ -4,14 +4,12 @@
 
 ;;; Code:
 
-;; Add node_modules to path
 (use-package add-node-modules-path
   :ensure t
   :hook ((typescript-mode . add-node-modules-path)
          (typescript-tsx-mode . add-node-modules-path)
          (js2-mode . add-node-modules-path)))
 
-;; Tree-sitter based TypeScript modes (Emacs 29+)
 (use-package typescript-ts-mode
   :mode (("\\.ts\\'" . typescript-ts-mode)
          ("\\.tsx\\'" . tsx-ts-mode))
@@ -19,37 +17,12 @@
   (setq typescript-ts-mode-indent-offset 2))
 
 
-;; Keep js2-mode for fallback/legacy JS
-(use-package js2-mode
-  :ensure t
-  :mode ("\\.js\\'" . js2-mode)
-  :config
-  (setq js2-basic-offset 2
-        js-indent-level 2
-        js-switch-indent-offset 2
-        indent-tabs-mode nil))
-
-;; JSX/TSX support
-(use-package tsx-ts-mode
-  :mode "\\.tsx\\'")
-
-;; Web-mode as fallback for complex JSX
-(use-package web-mode
-  :ensure t
-  :mode (("\\.jsx\\'" . web-mode))
-  :config
-  (setq web-mode-markup-indent-offset 2
-        web-mode-css-indent-offset 2
-        web-mode-code-indent-offset 2
-        web-mode-content-types-alist '(("jsx" . "\\.jsx?\\'"))))
-
 ;; Smart parens
 (when (featurep 'smartparens)
   (add-hook 'typescript-mode-hook #'smartparens-mode)
   (add-hook 'typescript-ts-mode-hook #'smartparens-mode))
 
 
-;; REPL for Node.js
 (use-package nodejs-repl
   :ensure t
   :bind (:map typescript-ts-mode-map
