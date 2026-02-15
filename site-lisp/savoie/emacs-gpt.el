2ff5ee16c17c6c368ff44ef5fceb51ce2c94e694
Matt Savoie <matthew.savoie@colorado.edu>
2026-02-15 10:00:00 -0700

    nuke some more things I don't use anymore.

--- site-lisp/savoie/emacs-gpt.el
+++ site-lisp/savoie/emacs-gpt.el
@@ -8,14 +8,13 @@
   (mhs/get-api-key "api.anthropic.com"))
 
 (defun mhs/get-openai-key ()
-  "Fetches the API key for OpenAI."
+  "Fetches the API key for OpenAI. gpt"
   (mhs/get-api-key "api.openai.com"))
 
 (use-package gptel
   :ensure t
   ;;  :pin melpa-stable
   :config
-  ;; expensive deep thinking claude-3-opus-20240229
 
   ;;Reddit comment on how make claude less verbose:
   ;; https://www.reddit.com/r/ClaudeAI/comments/1gn8bpf/comment/lw8l3q8/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
@@ -39,61 +38,6 @@ Help the user write idiomatic code, suggesting built-in functions when possible.
   (add-hook 'gptel-post-stream-hook 'gptel-auto-scroll)
   (gptel-make-gh-copilot "Copilot")
   )
-
-
-(use-package dall-e-shell
-  :ensure t
-  :defer t
-  :config
-  (setq dall-e-shell-openai-key 'mhs/get-openai-key))
-
-
-(use-package pcsv :ensure t)
-
-;; check out chatgpt integration.
-(use-package chatgpt-shell
-  :ensure t
-  :defer t
-  :config
-  (setq chatgpt-shell-model-version "gpt-3.5-turbo-0125"
-        chatgpt-shell-system-prompt 1
-        chatgpt-shell-openai-key 'mhs/get-openai-key)
-
-  ;; create a smart elisp tutor
-  ;; https://www.reddit.com/r/emacs/comments/1aj5did/using_llms_to_fill_the_semantic_search_gap/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-  (add-to-list 'chatgpt-shell-system-prompts
-	       '("Python Programming" . "The user is a python programmer. You treat their time as \
-precious. You do not repeat obvious things, including their \
-query.  You never apologize for confusions because it would waste \
-their time.  You use markdown liberally to structure responses. \
-Always show code snippets in markdown blocks with language \
-labels.  Don't explain code snippets unless asked.  Whenever you \
-output updated code for the user, only show diffs, instead of \
-entire snippets."))
-
-  (add-to-list 'chatgpt-shell-system-prompts
-	       '("Emacs Guru" . "You are an expert in all things Emacs. Help the user write \
-ideomatic code when asked.  Also suggest built-in functions when \
-possible.  End your response by approximating the coherence of \
-your reply from 0 to 100%.")))
-
-
-;; Set up copilot
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name
-        "straight/repos/straight.el/bootstrap.el"
-        (or (bound-and-true-p straight-base-dir)
-            user-emacs-directory)))
-      (bootstrap-version 7))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
 
 
 (provide 'emacs-gpt)
