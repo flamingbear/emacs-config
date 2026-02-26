d34796a5b87660bb963b9f6234d0f95134a39426
Matt Savoie <matthew.savoie@colorado.edu>
2026-02-26 09:46:00 -0700

    Adding claude-code-ide, but haven't played with it yet.

--- site-lisp/savoie/emacs-gpt.el
+++ site-lisp/savoie/emacs-gpt.el
@@ -39,5 +39,10 @@ Help the user write idiomatic code, suggesting built-in functions when possible.
   (gptel-make-gh-copilot "Copilot")
   )
 
+(use-package claude-code-ide
+  :vc (:url "https://github.com/manzaltu/claude-code-ide.el" :rev :newest)
+  :bind ("C-c C-'" . claude-code-ide-menu) ; Set your favorite keybinding
+  :config
+  (claude-code-ide-emacs-tools-setup)) ; Optionally enable Emacs MCP tools
 
 (provide 'emacs-gpt)
