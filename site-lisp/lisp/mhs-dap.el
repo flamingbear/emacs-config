c36e7d74986fdc544030e6a599219f4fa6016489
Matt Savoie <matthew.savoie@colorado.edu>
2026-04-10 14:08:48 -0600

    Better stepping

--- site-lisp/lisp/mhs-dap.el
+++ site-lisp/lisp/mhs-dap.el
@@ -39,7 +39,7 @@
          :restart t
          :sourceMaps t
          :smartStep t
-         :skipFiles ["<node_internals>/**" "${workspaceFolder}/node_modules/**/*.js"]
+         :skipFiles ["<node_internals>/**" "**/node_modules/**"]
          :name "Attach to Harmony"))
 
   ;; Attach to any running Node process by PID
@@ -50,8 +50,57 @@
          :processId "${command:PickProcess}"
          :sourceMaps t
          :smartStep t
-         :skipFiles ["<node_internals>/**" "${workspaceFolder}/node_modules/**/*.js"]
+         :skipFiles ["<node_internals>/**" "**/node_modules/**"]
          :name "Node :: Attach by PID")))
 
 (provide 'mhs-dap)
 ;;; mhs-dap.el ends here
+
+;; launch.json?
+;; {
+;;   "version": "0.2.0",
+;;   "configurations": [
+;;     {
+;;       "type": "node",
+;;       "request": "launch",
+;;       "name": "Debug Harmony Service",
+;;       "skipFiles": ["<node_internals>/**"],
+;;       "program": "${workspaceFolder}/services/harmony/app/server.ts", // Adjust if your entry point is different
+;;       "runtimeArgs": ["-r", "ts-node/register"],
+;;       "envFile": "${workspaceFolder}/services/harmony/.env", // Loads your environment variables
+;;       "sourceMaps": true,
+;;       "smartStep": true,
+;;       "console": "integratedTerminal",
+;;       "outFiles": ["${workspaceFolder}/services/harmony/dist/**/*.js"]
+;;     },
+;;     {
+;;       "type": "node",
+;;       "request": "attach",
+;;       "name": "Attach to Process",
+;;       "port": 9229,
+;;       "skipFiles": ["<node_internals>/**"]
+;;     }
+;;   ]
+;; }
+;; launch json for emacs?
+;; {
+;;   "version": "0.2.0",
+;;   "configurations": [
+;;     {
+;;       "type": "node",
+;;       "request": "launch",
+;;       "name": "Harmony: Debug Locator Middleware",
+;;       "skipFiles": ["<node_internals>/**"],
+;;       "program": "${workspaceFolder}/services/harmony/app/server.ts",
+;;       "runtimeArgs": [
+;;         "-r", "ts-node/register"
+;;       ],
+;;       "args": [],
+;;       "cwd": "${workspaceFolder}/services/harmony",
+;;       "envFile": "${workspaceFolder}/services/harmony/.env",
+;;       "protocol": "inspector",
+;;       "console": "integratedTerminal",
+;;       "sourceMaps": true
+;;     }
+;;   ]
+;; }
