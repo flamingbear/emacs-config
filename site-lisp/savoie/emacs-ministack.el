df126f177a9feff59d7b5ab55291ee1476f4c94f
Matt Savoie <matthew.savoie@colorado.edu>
2026-04-27 13:22:21 -0600

    Get rid of vertico-resize
    That bouncing around was crazy and irritating.

--- site-lisp/savoie/emacs-ministack.el
+++ site-lisp/savoie/emacs-ministack.el
@@ -17,7 +17,6 @@
 
   ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
   (setq vertico-cycle t)
-  (setq vertico-resize t)
   )
 
 ;; Persist history over Emacs restarts. Vertico sorts by history position.
