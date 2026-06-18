09868361f28f1ab3259c9c31d1e4814c57369ac9
Matt Savoie <matthew.savoie@colorado.edu>
2026-06-18 09:20:51 -0600

    fix ansi colors

--- site-lisp/lisp/mhs-extends.el
+++ site-lisp/lisp/mhs-extends.el
@@ -439,10 +439,24 @@ following the prefix character"
       (ansi-color-apply-on-region (point-min) (point-max))))
   (add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
 
+  ;; (defun destructive-display-ansi-colors ()
+  ;;   (interactive)
+  ;;   (ansi-color-apply-on-region (point-min) (point-max)))
+  ;; )
+  ;; Because you have muscle memory...just rename it also
   (defun destructive-display-ansi-colors ()
+    "Show ANSI colors without marking buffer as modified."
     (interactive)
-    (ansi-color-apply-on-region (point-min) (point-max)))
-  )
+    (let ((inhibit-read-only t))
+      (ansi-color-apply-on-region (point-min) (point-max)))
+    (set-buffer-modified-p nil))
+
+  (defun display-ansi-colors ()
+    "Show ANSI colors without marking buffer as modified."
+    (interactive)
+    (let ((inhibit-read-only t))
+      (ansi-color-apply-on-region (point-min) (point-max)))
+    (set-buffer-modified-p nil))
 
 
 ;; found here https://stackoverflow.com/a/33456622/66100
